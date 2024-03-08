e9d4ea853ad8acbb29c0189ffa891b9fc79f3c6d
cristxbal_vc <cristobalvalencia3002@gmail.com>
2024-03-08 16:16:45 -0500

    inicializador

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" class="img">
+  <HelloWorld msg="DevCristobalvc"/>
 </template>
 
 <script>
@@ -23,4 +23,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.img{
+  width: 300px; /* Cambia a tu tamaño deseado */
+  height: auto; /* Mantiene la proporción de la imagen */
+}
+
 </style>
