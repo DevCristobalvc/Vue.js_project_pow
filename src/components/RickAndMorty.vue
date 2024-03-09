e9649a6b5f5465940866c9ab194a3bdd026effde
cristxbal_vc <cristobalvalencia3002@gmail.com>
2024-03-08 20:21:26 -0500

    se agrega vista princiapal

---
+++ src/components/RickAndMorty.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="hello">
+      <h1>{{ nombre }}</h1>
+      <div v-for="item in items" :key="item.id">
+        <div>
+          <h1>{{ item.name }}</h1>
+          <img :src="item.image" alt="">
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onBeforeMount, ref } from "vue";
+  import axios from "@/axios";
+  
+  const nombre = ref("DevCristobalvc")
+  const items = ref("");
+    const valoresRick = async () => {
+      try{
+          const response = await axios.get("/character/");
+          items.value = response.data.results;
+          console.log(items.value);
+        } catch (error) {
+          console.error("Error de datos", error);
+        }
+    }
+  
+  onBeforeMount(()=>{
+    valoresRick()
+  })
+  
+  </script>
